c11f96bee2fcff443a7b0323883f3554afd0b00d
Ariana23 <arianavargas235@hotmail.com>
2020-04-10 14:11:53 -0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+$conn = mysqli_connect(
+	'localhost',
+	'root',
+	'',
+	'php_mysql_crud'
+);
+
+
+
+?>
---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=devide=width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible"content="ie=edge">
+	<title>AriAdmin</title>
+	<!-- BOSTSTRAP 4-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<!--FONT AWESOME 5 -->
+	<script src="https://kit.fontawesome.com/525aeb1851.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+	<div class="container">
+	<a href="index.php" class="navbar-brand">Sistema de Ticket</a>
+	</div>
+</nav>
